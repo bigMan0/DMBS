3d9b655508b1bd384b414c97740bcb91630cfdc8
SaifOver9000 <123909641+SaifOver9000@users.noreply.github.com>
2024-03-14 22:18:08 -0300

    added the API
    added the api requests in the server.js file

--- server.js
+++ server.js
@@ -31,24 +31,45 @@ const {getBookingbyId} = require('./routes/get-booking-by-id');
 const {addpatient} = require('./routes/add-patient');
 const {adddepartment} = require('./routes/add-departments');
 const {addrecord} = require('./routes/add-record');
-const {addprocedeur} = require('./routes/add-procedeurs');
+const {addprocedeur} = require('./routes/add-procedeur');
 const {addbooking} = require('./routes/add-booking');
 
+//DELETE
+const {removePatient} = require('./routes/remove-patient');
+const {removeDepartment} = require('./routes/remove-department');
+const {removeRecord} = require('./routes/remove-record');
+const {removeProcedeur} = require('./routes/remove-procedeur');
+const {removeBooking} = require('./routes/remove-booking');
 
 //getting all rows of the table.
 app.get('/Patients', (req, res) => getAllPatients(req, res, supabase));
 app.get('/Department', (req, res) => getAllDepartments(req, res, supabase));
 app.get('/Records', (req, res) => getAllRecords(req, res, supabase));
 app.get('/Procedures', (req, res) => getAllProcedeurs(req, res, supabase));
+app.get('/Bookings', (req, res) => getAllBookings(req, res, supabase));
 
 // getting rows by id.
 app.get('/Records/:health_card', (req, res) => getPatientRecords(req, res, supabase));
 app.get('/Patients/:health_card', (req, res) => getPatientById(req, res, supabase));
 app.get('/Bookings/:health_card', (req, res) => getBookingbyId(req, res, supabase));
+app.get('/Department/:staff_number', (req, res) => getDepartmentbyid(req, res, supabase));
 
 
 //adding new rows to the table
-app.post('/Patients', (req, res) => addpatients(req, res, supabase));
+app.post('/Patients', (req, res) => addpatient(req, res, supabase));
+app.post('/Department', (req, res) => adddepartment(req, res, supabase));
+app.post('/Records', (req, res) => addrecord(req, res, supabase));
+app.post('/Procedures', (req, res) => addprocedeur(req, res, supabase));
+app.post('/Bookings', (req, res) => addbooking(req, res, supabase));
+
+
+//removing rows from the table
+app.delete('/Patients/:health_card', (req, res) => removePatient(req, res, supabase));
+app.delete('/Department/:staff_number', (req, res) => removeDepartment(req, res, supabase));
+app.delete('/Records/:health_card', (req, res) => removeRecord(req, res, supabase));
+app.delete('/Procedures/:health_card', (req, res) => removeProcedeur(req, res, supabase));
+app.delete('/Bookings/:health_card', (req, res) => removeBooking(req, res, supabase));
+
 
 app.listen(port, () => console.log(`Server Listening on port ${port}`))
 
